4e7b36e4d5f4233d8e6e2b75c8b77cbd61e92e20
jeff-vincent <jeff.d.vincent@gmail.com>
2020-02-23 14:03:14 -0800

    mas stuffs

--- basic_lstm.py
+++ basic_lstm.py
@@ -1,10 +1,16 @@
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Embedding
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.corpus import stopwords
 import re
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Embedding
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+MAX_SEQ_LENGHT = 2128
+
  
 def clean_review(text):
     # Strip HTML tags
@@ -20,27 +26,34 @@ def clean_review(text):
 
 
 # load dataframe in pandas
-df = pd.read_csv('labeledTrainData.tsv', sep='\t', quoting=3)
+# NOTE: skipping sketchy lines
+df = pd.read_csv('Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv', error_bad_lines=False)
+print(df)
 
 # clean text to be analyzed
-df['cleaned_review'] = df['review'].apply(clean_review)
+df['cleaned_review'] = df['reviews.text'].apply(clean_review)
 
 # sklearn's `train_test_split()` -- standard for breaking 
 # labeled data into train/test groups
-X_train, X_test, y_train, y_test = train_test_split(df['cleaned_review'], df['sentiment'], test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(df['cleaned_review'], df['reviews.doRecommend'], test_size=0.2)
  
+
+# Define Vectorizer for converting words to Vectors
 vectorizer = CountVectorizer(binary=True, stop_words=stopwords.words('english'), 
                              lowercase=True, min_df=3, max_df=0.9, max_features=5000)
+
+# convert training data to one-hot vectors
 X_train_onehot = vectorizer.fit_transform(X_train)
+
 
 
 # Sequential model for nlp baybee!!
 model = Sequential()
 
-# Define Embedding layer
+# Define Embedding layer -- vector to tensor
 model.add(Embedding(len(vectorizer.get_feature_names()) + 1,
                     64,  # Embedding size
-                    input_length=MAX_SEQ_LENGHT))
+                    ))
 
 # Define LSTM (Long Short Term Memory) layer
 model.add(LSTM(64))
@@ -61,6 +74,10 @@ print(model.summary())
 model.fit(X_train_onehot[:-100], y_train[:-100], 
           epochs=2, batch_size=128, verbose=1, 
           validation_data=(X_train_onehot[-100:], y_train[-100:]))
+
+# Test the Model
+scores = model.evaluate(vectorizer.transform(X_test), y_test, verbose=1)
+print("Accuracy:", scores[1])
 
 
 
